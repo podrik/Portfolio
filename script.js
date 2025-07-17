8529f5b362a9a6be1c7ee77b9af590eb434ea55f
podrik <kishanchand.iitkgp@gmail.com>
2025-07-17 12:45:08 +0530

    Fixed dark mode toggle

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const toggleButton = document.getElementById('darkModeToggle');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById('darkModeToggle');
+
+    toggleButton.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+});
